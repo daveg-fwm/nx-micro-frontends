3a2b43bc4c88c6919426ababc58c83b7852a4edf
Dave Green <david.green@cloudsmiths.ai>
2026-05-13 11:41:08 +0200

    feat: add dogs app

--- apps/dogs/src/App.tsx
+++ apps/dogs/src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="content">
       <h1>
-        <span className="mb-3 block">DOGS OR CATS</span>Rsbuild with React
+        <span className="mb-3 block">DOGS</span>Rsbuild with React
       </h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
